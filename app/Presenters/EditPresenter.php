eff9c09197127b5d7f25fac324f9de8b3890d02d
grebeniceks <sgrebenicek@gmail.com>
2023-06-18 21:43:57 +0200

    commit

--- app/Presenters/EditPresenter.php
+++ app/Presenters/EditPresenter.php
@@ -4,6 +4,7 @@ namespace App\Presenters;
 
 use Nette;
 use Nette\Application\UI\Form;
+
 final class EditPresenter extends Nette\Application\UI\Presenter
 {
     public function __construct(
@@ -17,9 +18,8 @@ final class EditPresenter extends Nette\Application\UI\Presenter
     protected function createComponentBookForm(): Form
     {
 
-    $form = new Form;
+        $form = new Form;
 
-        $form->addHidden('id');
         $authors = $this->database->table('authors')
             ->select('CONCAT(name, " ", surname) AS full_name, id')
             ->order('full_name')
@@ -33,8 +33,8 @@ final class EditPresenter extends Nette\Application\UI\Presenter
         $form->addInteger('year', 'Year:')
             ->setRequired()
             ->addRule(function ($value) {
-                return $value->getValue() <= 2023 && $value->getValue() >=1800;
-            }, 'Input a value between 0 and 2023');
+                return $value->getValue() <= 2023 && $value->getValue() >= 1800;
+            }, 'Input a value between 1800 and 2023');
         $form->addInteger('pages', 'Pages:')
             ->setRequired();
 
@@ -44,13 +44,82 @@ final class EditPresenter extends Nette\Application\UI\Presenter
         return $form;
     }
 
+    protected function createComponentAuthorForm(): Form
+    {
+        $form = new Form();
+
+        $form->addText('name', 'Name:')
+            ->setRequired();
+        $form->addText('surname', 'Surname:')
+            ->setRequired();
+
+        $form->addSubmit('send', 'Save');
+        $form->onSuccess[] = [$this, 'authorFormSucceeded'];
+
+        return $form;
+    }
+
     public function bookFormSucceeded(array $data): void
     {
-        $book = $this->database
-            ->table('books')
-            ->insert($data);
+        $bookId = $this->getParameter('bookId');
 
-        $this->flashMessage('Book was published', 'success');
+        if ($bookId) {
+            $book = $this->database
+                ->table('books')
+                ->get($bookId);
+            $book->update($data);
+
+        } else {
+            $book = $this->database
+                ->table('books')
+                ->insert($data);
+        }
+
+        $this->redirect('Book:show', $book->id);
+    }
+
+    public function authorFormSucceeded(array $data): void
+    {
+        $authorId = $this->getParameter('authorId');
+
+        if ($authorId) {
+            $author = $this->database
+                ->table('authors')
+                ->get($authorId);
+            $author->update($data);
+
+        } else {
+            $author = $this->database
+                ->table('authors')
+                ->insert($data);
+        }
+
+        $this->redirect('Author:show', $author->id);
+    }
+
+
+    public function renderEdit(int $bookId): void
+    {
+
+        $book = $this->database->table('books')->get($bookId);
+
+        if (!$book) {
+            $this->error('Book not found');
+        }
+
+        $this->template->book = $book;
+
+    }
+
+    public function renderEditAuthor(int $authorId): void
+    {
+        $author = $this->database->table('authors')->get($authorId);
+
+        if (!$author) {
+            $this->error('Author not found');
+        }
+
+        $this->template->author = $author;
     }
 
 }
